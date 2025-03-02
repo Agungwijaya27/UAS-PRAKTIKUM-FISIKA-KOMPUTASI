1918ea3d0e54830255bb307a386e92b030397c15
Agung Wijaya Temiesela <114717595+Agungwijaya27@users.noreply.github.com>
2025-03-03 05:13:28 +0700

    Update and rename Runing - Agung Wijaya Temiesela.py to Hasil Runing.py

--- Hasil Runing.py
+++ Hasil Runing.py
@@ -1,7 +1,7 @@
 Python 3.10.1 (tags/v3.10.1:2cd268a, Dec  6 2021, 19:10:37) [MSC v.1929 64 bit (AMD64)] on win32
 Type "help", "copyright", "credits" or "license()" for more information.
 
-= RESTART: C:\Users\HP\Downloads\UAS-PRAKTIKUM-KOMPUTASI-main\UAS-PRAKTIKUM-KOMPUTASI-main\No 2. Memplot titik pada parabola.py
+= RESTART: C:\Users\HP\Downloads\No 2. Memplot titik pada parabola.py
 Jarak Horizontal Maksimum =  5.1020408163265305  m
 Jarak Vertikal Maksimum =  2.5510204081632657  m
 Waktu Mencapai Jarak Horizontal Maksimum =  1.4430750636460152  s
